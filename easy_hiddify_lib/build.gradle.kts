990f477a1392ae7d750e13677127e82e9e18856f
vaceslavpereaslov <perejaslov9200@gmail.com>
2026-08-09 22:44:20 +0300

    - fixed: Unnecessary imports have been removed from the library

--- easy_hiddify_lib/build.gradle.kts
+++ easy_hiddify_lib/build.gradle.kts
@@ -35,13 +35,7 @@ kotlin {
 }
 
 dependencies {
-    implementation(libs.androidx.appcompat)
     implementation(libs.androidx.core.ktx)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(libs.androidx.junit)
-
 
     api(files("libs/hiddify-core.aar"))
 }
